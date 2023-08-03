766feba9d75624d1484b9aad227cce4d50de4abe
Marshall <marshall.peters01@gmail.com>
2023-08-03 19:46:55 -0400

    added character login and creation components

--- client/src/pages/CharacterCreate.jsx
+++ client/src/pages/CharacterCreate.jsx
@@ -1,12 +1,40 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function CharacterCreate() {
+  const history = useHistory();
+  const [name, setName] = useState('');
+  const [selectedClass, setSelectedClass] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleClassChange = (event) => {
+    setSelectedClass(event.target.value);
+  };
+
+  const handleCreateCharacter = () => {
+    history.push('/introduction');
+  };
+
   return (
     <div>
-      <h1>Choose a class!</h1>
-      <Link to={{ pathname: '/introduction'}}>
-        <button>Begin Adventure</button>
-      </Link>
+      <h2>Create Your Character</h2>
+      <div>
+        <label htmlFor="name">Adventurer Name:</label>
+        <input type="text" id="name" value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        <label htmlFor="class">Class:</label>
+        <select id="class" value={selectedClass} onChange={handleClassChange}>
+          <option value="">Select a Class</option>
+          <option value="Fighter">Fighter</option>
+          <option value="Wizard">Wizard</option>
+          <option value="Ranger">Ranger</option>
+        </select>
+      </div>
+      <button onClick={handleCreateCharacter}>Create Character</button>
     </div>
   );
 }
