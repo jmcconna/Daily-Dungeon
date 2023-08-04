056107d20d5b0badbc0bf90117d3f1bf33e35b4e
JTrue <truehitt@gmail.com>
2023-08-04 14:47:31 -0400

    Prep for Heroku Deployment Swapped everything over to CRA. Vite and Heroku dont seem to like each other. Swapped database to Mongo Atlas so we can access and adjust the relationships and shouldn't need to seed once on heroku. Minor bug fixes due to some updates to character models.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dailydungeon');
+
+    mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        })
+    
 
 module.exports = mongoose.connection;
