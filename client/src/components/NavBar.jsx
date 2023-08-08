6bda2a8b6a24305e7259988411bd3fed7ecd097a
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-07 22:14:11 -0700

    trying to fix the background

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import ToggleMusic from './ToggleMusic';
 function NavBar() {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light bkrgound">
             <a class="navbar-brand" href="/">Daily Dungeon</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
