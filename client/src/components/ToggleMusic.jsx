2ea6d1dd92994a869de14ebc8c4359a570bf239a
James McConnaughey <jbmcconnaughey@gmail.com>
2023-08-07 21:01:16 -0400

    improved styling for the music button

--- client/src/components/ToggleMusic.jsx
+++ client/src/components/ToggleMusic.jsx
@@ -11,7 +11,7 @@ state = {
 	audio: new Audio(song),
 
 	// Set initial state of song
-	isPlaying: true,
+	isPlaying: false,
 };
 
 // Main function to handle both play and pause operations
@@ -36,16 +36,12 @@ playPause = () => {
 render() {
 	return (
 	<div>
-		{/* Show state of song on website */}
-		<p>
-		{this.state.isPlaying ?
-			"Song is Playing" :
-			"Song is Paused"}
-		</p>
 
 		{/* Button to call our main function */}
 		<button onClick={this.playPause}>
-		Play | Pause
+		{this.state.isPlaying ?
+			"Music ON" :
+			"Music OFF"}
 		</button>
 	</div>
 	);
