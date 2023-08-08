02621f4e2acca20ce44f0475c8a99d61212201ce
James McConnaughey <jbmcconnaughey@gmail.com>
2023-08-07 22:40:04 -0400

    attempted to update the gold and XP of the character at the end of combat in EndMenu.js

--- client/src/pages/Combat/components/EndMenu/EndMenu.js
+++ client/src/pages/Combat/components/EndMenu/EndMenu.js
@@ -1,16 +1,49 @@
 import styles from './styles.module.css';
 import { useNavigate } from 'react-router-dom';
+import { GET_CHARACTER_QUERY } from '../../../../utils/queries';
+import { UPDATE_CHARACTER_MUTATION } from '../../../../utils/mutations';
+import { useMutation, useQuery } from '@apollo/client';
 
 const EndMenu = ({ winner, character }) => {
   const navigate = useNavigate();
+  const characterId = JSON.parse(localStorage.getItem('DD_session')).character._id;  
+  
+  const [mutateGoldXP, { loading, error}] = useMutation(UPDATE_CHARACTER_MUTATION);
+const updateGoldXP = async (data) => {
+  try {
+    //try to update the gold and XP of the character in the db  
+    const { data } = await mutateGoldXP({
+      variables: {
+        _id: characterId,
+        gold: data.newGold,
+        experience: data.newExperience
+      },})
+
+} catch (error) {
+  console.error('Error retrieving character data', error.message);
+}}
   
   if (winner.name === character.name) {
-    
+
+    //update local storage
     character.experience += 25;
     character.gold += 10;
     const DD_session = JSON.parse(localStorage.getItem('DD_session'));
     DD_session.character = character;
     localStorage.setItem('DD_session', JSON.stringify(DD_session));
+
+
+    //retrieve the current gold and XP of the character in the db
+    const {loading, error, data} = useQuery(GET_CHARACTER_QUERY, {
+        variables: {
+          _id: characterId,
+        },
+      });
+      let newGold = data.gold +10;
+      let newExperience = data.experience +25;
+      updateGoldXP({ characterId, newGold, newExperience});
+    
+
   } else {
     localStorage.removeItem('gameState')
     localStorage.removeItem('randomTilesGenerated')
